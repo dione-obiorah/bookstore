f7f4049bb113c8fabde3a89894bda8fbba7f4736
Jessica-Dione Obiorah <jessica-dione@Jessica-Diones-MacBook-Pro.local>
2022-11-03 12:34:58 +0000

    tester

---
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.weCode.bookStore.controller;
+
+import com.weCode.bookStore.model.Book;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+    @GetMapping()
+    public ResponseEntity<List<Book>> getBook(){
+        Book book = Book.builder().title("My first book").build();
+        List<Book> books = new ArrayList<>();
+        books.add(book);
+        return ResponseEntity.ok(books);
+    }
+}
---
+++ src/main/java/com/weCode/bookStore/model/Book.java
@@ -0,0 +1,14 @@
+package com.weCode.bookStore.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private String title;
+}
